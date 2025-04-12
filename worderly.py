a6090e75505359e593f53e8fec2de2690cc2e21d
joelbaldapan <baldapan.joelangelo@gmail.com>
2025-04-12 16:51:02 +0800

    feat: add grid printing and middle word placement

--- worderly.py
+++ worderly.py
@@ -2,6 +2,10 @@ import random
 import itertools
 
 
+# ****************
+# WORD CREATION
+# ****************
+
 def read_word_file(word_path):
     try:
         with open(word_path, "r") as file:
@@ -85,6 +89,59 @@ def generate_word_list():
     return result  
 
 
+# ****************
+# DISPLAY
+# ****************
+def print_grid(grid):
+    for row in grid:
+        print(" ".join(cell if cell else "." for cell in row))
+
+
+# ****************
+# GRID CREATION
+# ****************
+
+def create_empty_grid(height, width):
+    return [[None] * width for _ in range(height)]
+
+
+def place_middle_word(grid, word, letter_coords):
+    height = len(grid)
+    width = len(grid[0])
+    word_len = len(word)
+
+    diag_space = word_len * 2 - 1
+    start_row = (height - diag_space) // 2
+    start_col = (width - diag_space) // 2
+
+    if (
+        start_row < 0
+        or start_col < 0
+        or start_row + diag_space > height
+        or start_col + diag_space > width
+    ):
+        print(f"Error: Grid ({height}x{width}) too small for placing '{word}' (LENGTH {word_len}) diagonally with spacing.")
+        return None
+
+    letter_coords.clear()
+    middle_word_coords = set()
+
+    row, col = start_row, start_col
+    for letter in word:
+        grid[row][col] = letter.upper()
+        coord = (row, col)
+        letter_lower = letter.lower()
+        letter_coords.setdefault(letter_lower, []).append(coord)
+        middle_word_coords.add(coord)
+        row += 2
+        col += 2
+
+    return middle_word_coords
+
+
+# ****************
+# MAIN LOGIC
+# ****************
 
 settings = {
     "grid": {"height": 15, "width": 25},
@@ -95,14 +152,27 @@ settings = {
 }
 
 def main():
-    # Create word list
+    # CREATE WORD LIST
     word_setup_result = generate_word_list()
-    print(word_setup_result)
+    
+    if word_setup_result is None:
+        raise ValueError("Failed to set up word list!")
 
-    # Create grid
-    ...
+    middle_word, words_to_place = word_setup_result
 
-    # Gameplay
+    # CREATE GRID
+    grid = create_empty_grid(settings["grid"]["height"], settings["grid"]["width"])
+    print_grid(grid)
+    
+    # TODO: separate functioanlities for setting letter coords dictionary
+    print()
+    letter_coords = {}
+    middle_word_coords = place_middle_word(grid, middle_word, letter_coords)
+    print_grid(grid)
+    print(middle_word_coords)
+    
+
+    # GAMEPLAY
     ...
 
 if __name__ == "__main__":
