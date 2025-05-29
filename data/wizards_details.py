5a923dad02d52017bb4b32cf6cf90e45d64028fc
joelbaldapan <baldapan.joelangelo@gmail.com>
2025-05-30 04:01:45 +0800

    refactor: create dataclasses data

--- data/wizards_details.py
+++ data/wizards_details.py
@@ -1,265 +1,92 @@
-# ************************************
-#           WIZARD SPRITES
-#   Reference: TERRARIA SPRITES :)
-#   - Alvin, choosing the sprites!
-# ************************************
+from dataclasses import dataclass
 
-WIZARDS_ART = [
-    """
-                █▓▓▓▓▓█
-              ▓▓▓▓▓▓▒░░░▒▒▓
-                ▓▓▓▒░    ▒▒
-                  ██▓▒░   ▒██
-               █▓▓▓▓▓▓▓▓░ ░▓▓█
-          █▓████▓▒░   ░▓▓▒░ ▒██
-        █▓▒░▒▓▓▒▒▒▒▒▒░   ░  ▒██
-    ▓▓▓▓▒ ░▓▒ ░▒░  ░▓██▓░ ░▓███
-    ▓▓▒  ░░ ░▓▓█▓░░ ░▒▒▒   ▒▓██▓▓▓▓
-     ▓▓▓▒  ▒▓███▓░       ▒  ▒█▒ ░▓▓
-       █▒▒▓███████▒░    ░▓░ ▒█▓▒░
-        █████▒▓▓▒ ▒████████████▓░
- █▓▓▓▓▓▓███▒▒▓▓▒▒▓▓▒   ░   ▒▓█▓
-▓▓▒    ▒▓█▓  ▒▓█▒ ░▓▓▒   ▒▒▓███▒▒▒▒
-  ▓▓▓▒▓█▓    ░▒██▓░    ▒    ▒█▒  ▒▒
-      ███▒░░  ░████▓▓▒░░░▒▓██████
-    ███▒▒▒░ ░█▒ ▒█▓   ▒█▒ ▒█▒ ▒██
-    ███▒░ ░█▓░  ░█▒ ░██▓████▒ ░██
-     ████████████▓░ ▒█▓░▓██▓███
-             ██▓▒ ░░▓█▒ ▒█▒ ▒██
-           ███▓░░▒▓████▒██▓▒▒██
-         ██▓▓▒▓██▓▓▓▒ ░▒▓█▓▒▓██
-         ██████████████████████
-""",
-    """
-                        ░▒░
-                 ████▓▒▒░░▒▒▒
-          ▓ ▒████░░          ██▒░▓
-          ██▒░░░░▒▒░           ▓██
-          ██▒░░▒▒▒▒░  ▒▒   ▒▒  ▒██
-       ███░░░▒░         ░▒     ▒██
-     █████▒▒        █████████   ██
-     ███▒▒░░   ████▓░░░░██▓░░█████
-     ███▓▓▒▒   ████     ▒█   ███
-     ▓░███▓█░  ▒▒██▓▒▒▒▒██▓▒▒██▓
-  ▒▓▓  ░▒▒▓▓▓▓▒  ▒▒▓█████████   ▒█
-░▓░         ▒███▓░░▓███████▓▒   ██
-▓█░         ░██▓▓▒▒▓▓▓▓▓▒░░▒░  ░██
-▓█          ░▓███░▒░░░▒▒░░▒██▓▓▒██
-  ░  ▓▓     ▒██▓▓██▓▓▓▓▓█████▓▓▒
-     ██▓░░  ▓████░▒▒▓▓▓▓▒▒▓██  ░██
-     ▒▒░      ░██  ░░░▒▒░ ▒██  ░██
-  ▓█▒        ░▒██   ░░░    ██░░▒██
-  ███   ░░▓▓▓▓▓▓▓░░      ░▓██▓▓▒▒▒
-  ████████▒▒▒▒▒░░░░░▒▒██▒░▓██
-       ███░░   ▒░     ██   ██░
-    ████▒▒░░▒▒      ██████   ███
-     ██████████████████████████▓
-""",
-    """
-      █████   ██████▓████
-     ███████  ██████████▓▓█
-     ███░░▒▓███▓  ░░░░░░░█▓▓█
-     ████▒░▓██         ░░░░███
-       ███▓▓██            ░██▓
-       ███████            ░░██▓
-  ██     ███████            ░████
-████████████▓▓▓███        ░███████
- ██░░█████████░░████░   ▒█▓▒▓█████
-   ██░░░░▓▓███▓▓  ███▓▓▓███  ██
-    ████▓▓▒▓████▓▓██▓▓▓▓▓██▓▓███
-  █████░ ▒██████▓▓██▒  ░▒██████
-   ██▒░   ░▒██▓▓░░░░     ░▒██▓▓
-   ███████████▓▓████░   ▒████▓▓
-    ███   ░░██▓███░ ▓████████▓▓
-   ██▒▒▓█▓▓▓░░██▓▓▒▒   ░▓██▒▓▓██
-     ██▓▒░    ██░░▓▓███████░▒██
-       ███████████░░██▓░▓████
-         ████▓░░    ▓█▒ ▒██
-       ███▒▒░░░░███████████
-     ███▒▒░▒███████████████
-       █████████▓▓▒▒░ ▓█▓ ▒██
-         ████████████████████
-""",
-    """
-     ▓▓▓▒▒▓▓█     ▓▓▒▒▒▓▓
-     ▒▓▓░ ░▒▒███  █████████
-     ▓▓▓░   ░▒▒▓▓█▓▒▒▒▒▒▒▒▒██
-      ▓▒▒▒░    ░▒▒░░░▒░░░░░▓▓██
-        ▓▒▓█▒ ░░       ▒▒▒░  ██
-        ▒▒▓█▓▒░   ░█▓▒░    ▒▓██
- ▓▒▒▓▓▒▒  ░▒▓█▒     ░█▓▒▒▓▓▓▓██
-▒▒  ▒▒▓▓░ ▒▓▓█▓▒░░       ▓▒  ██ ▓▒▒
-▒▒    ▒▒░░▒▒▓█▓▓▓▒▒░░    ▓▒  ██▒▒▒▒
-▓▓▒▒    ░░  ░▒▓█▓▓▓▒▒░   █▓  ██  ▒▒
- ▓▒▒▓▓░░  ░▒▒▒░ ░██████████████▓▓▒▒
-▒▒    ▒▓▒▒░   ░█▒   ░░▒▓▒░░ ░██▒▒▓
- ▒▒▒▓▓░   ▓█████▓▒░    ░     ██  ▒▒
-  ▓▓▓▓▓▓██▓▓▓▓▓█▓░▒▒░ ░▓▒  ▒▒██▒▒▒▓
-     ▓▓▓▓▓▒▒░   ░█▓▒▓▓▒░▒▓▓██ ░███
-   ▓▓▓▒▒░       ░█▒ ░▒░  ▒▓█▓  ██
-     ▓▓▓▓▓      ░███▒     ░█▓  ██
-         █▒▓████▓▒▒▒▓█▒ ░██████
-           █  ██▒ ░▒▓▓▓██▓▓██▓
-          ▓███▒ ▒▓▓▓▒ ▒█▓ ░██
-        ██▒▓▓▓▓▓▓▒░   ░█▒  ███
-       █▓▓▒▒▒▒░░░░       █▓  ██
-          █████████████████████
-""",
-    """
-      ▒▒▒                  ▓▓
-     ▒░▒▒▒░     ██      ▓  ▓▓▓░
-    ▒▒░  ▒▒  █▓▓▓██  ░░▒▒░▓░▒▒▓▓
-  ▒▒▒░░ ░░▒█▓▒▒▒▒██░░░░░░░▒▒▒▒▒▓▓▓
-  ▒▒░░░░░  ▓▒ ░██▓▓▒▒░░  ░░▓█▒▒▒▓▓
-    ▒▒▒░░░▒▒▒░░▓▓▒▒▒▒░░  ░░██▓▒▓██
-        ████▓▒▒▒▒██████░░▓████████
-        ████▓  ░▒██  ▒▒▓▓░ ▓██████
-      ▒▒▒ ██▓░   ▒▒░░▒▒██▓▒████
-  ▒▒▒▒░ ░▒▓███▒░░░░░░██░░████▓▒▓▓▓
- ▒   ░░░░░░▓▓▓▓▓▓▓▓░░▓▓  ▓▓██▒░░░░▓
- ▒░░░░░▒▒▒▒▓▓█▓░░▒▒▓▓██▓▓████▒░░▒▒▓
-  ██▓▒▒░░ ▒█████▓▒▒      ▓▓██▒▒▒██
-   ███▒ ░▓▒░░▓▓▓▓░░░░  ░░░░▒▓████
-     █▒▒▓█▒ ░▒▓▓▓▒▒▒▒░░▒▒▒▒████
-  ▒▒▒▒▓█▒    ░▒▓▓▒▒  ▒▒  ▒▒░░▒▒▒
-  ▒▒▒▒▒░░ ░░░░▒▓▓██░░  ░░██▒▒▒▓▓
-   ▓▒▒▒▒▒░░ ░▒▒▓▓░░██▓▓████▓▒▓▓▓
-        ▒▒▒▒▒▒▒░░██▓▓  ▒▓███▓▓
-          ██▓  ░░██░░  ░░███
-        ██▓▒▒████░░░░██▓▓███
-          ███▓▓▓▒░░░░  ██  ▓██
-          ████████████████████
-""",
+from .wizards_art import WIZARDS_ART, WIZARDS_ART_SMALL
+
+
+@dataclass
+class WizardData:
+    name: str
+    art: str
+    small_art: str
+    color: str
+    starting_lives: int
+    combo_requirement: int | None
+    powerup_name: str
+    powerup_desc: str
+    description: str
+
+
+WIZARDS_DATA: list[WizardData] = [
+    WizardData(
+        name="Oldspella",
+        art=WIZARDS_ART[0],
+        small_art=WIZARDS_ART_SMALL[0],
+        color="bright_white",
+        starting_lives=5,
+        combo_requirement=None,
+        powerup_name="I Am Enough",
+        powerup_desc="<No powerup>",
+        description="I am an old soul with the bravado of a new journeyman. Although I may have no powerups, \
+          I am familiar with the words of this world, and I prefer to rely solely on my knowledge of the \
+          texts to save it.\n\n(This wizard contains no power-ups for players who prefer the classic \
+          Worderly Place gameplay.)",
+    ),
+    WizardData(
+        name="Wizard Dict",
+        art=WIZARDS_ART[1],
+        small_art=WIZARDS_ART_SMALL[1],
+        color="magenta",
+        starting_lives=4,
+        combo_requirement=3,
+        powerup_name="Hardbound Dict",
+        powerup_desc="Immune to damage from incorrect guesses for the next 2 turns.",
+        description="Yes... the rumors are indeed true. I used to be a dictionary, but a wizard turned me \
+          into a human. Words are literally and figuratively inside me... so it'll take a lot more to get \
+          me down.\n\n(This wizard is a bit more tough so he can defend himself from life reduction for \
+          one turn. Being a dictionary in your former life gives you a lot of pages to rip.)",
+    ),
+    WizardData(
+        name="Streambinder",
+        art=WIZARDS_ART[2],
+        small_art=WIZARDS_ART_SMALL[2],
+        color="blue",
+        starting_lives=3,
+        combo_requirement=3,
+        powerup_name="Tide of Renewal",
+        powerup_desc="Restore +1 Life.",
+        description="Greetings, traveler! I am a wizard devoted to healing the sick. With each cure I \
+          master, I edge closer to mending myself from this grueling ordeal.\n\n(This wizard has the \
+            option to heal himself but starts with less life since. He's cared so much for others, he \
+              had forgotten to care for himself...)",
+    ),
+    WizardData(
+        name="Fyaspella",
+        art=WIZARDS_ART[3],
+        small_art=WIZARDS_ART_SMALL[3],
+        color="red",
+        starting_lives=4,
+        combo_requirement=2,
+        powerup_name="Blazing Glyphs",
+        powerup_desc="Reveal 1 random word on the board.",
+        description="RAHHHH! I am a young and upcoming top student of the fya arts, and I've been \
+          quite the blaze! Gettin' rewarded for consistently stoking the flames, always, always, \
+          always!\n\n(This wizard is for those who prefer the pressure of consistently getting \
+          it right in order to get a big pay-off. Being a young wizard, you start with less \
+          health.)",
+    ),
+    WizardData(
+        name="Lettraseeker",
+        art=WIZARDS_ART[4],
+        small_art=WIZARDS_ART_SMALL[4],
+        color="green",
+        starting_lives=4,
+        combo_requirement=4,
+        powerup_name="Wildgrowth",
+        powerup_desc="Reveal 5-8 letters on the board.",
+        description="Ah, the garden of words bloom my curiosity. I am a wizard obsessed with the \
+          pieces of the puzzle, rather than the puzzle itself. Each letter is a mystical seed I've \
+          sown, scattering them across the soil of thought to let this garden grow.\n\n(This wizard \
+          is for those who prefer to compound a lead with a potential powerup. However, the dedication \
+          to such powerful magic makes you start with less life.)",
+    ),
 ]
-
-
-WIZARDS_ART_SMALL = [
-    """
-      ▓▓▓  ░▒
-       ██▓░  █
-     ▓▓█▓  ░▒░██
-  ▓▓▒░░▒ ▒▓▒  ░█▓
-  ▓░░ ░▓░      █▒
-  █▓███▓▒▓▓▓██▓██░
-▓▒  ▓ ▒█▒▓░  ▒▓█▓░▒
- █▓█▓░ ██▓░░░▒████░
-  █▓░▒█░▒ █▓▓██░▒█
-     ███████████
-""",
-    """
-      ▓███▒▒▒▒░░█
-    ██▓▓▒░     ▒▓█▓
-    █▓▒▒░      ░▓██
-  ██▓▒ ▓▓▓█████▓██▓
-   ██▓ ██░░▒█▒░██
-█▓  ░▒█▒▒▒█████  ██
- ▒▓█ ░█████████▓▓▓
-  ░░   █▓▒▒▒▒██ ░██
- █▓░▓██▓▓   ░████▓
-   ██████████████
-""",
-    """
-  ██████▓▓▓▒▒▓█
-   █▒ █        ██
-    ███       ░██
- █████▓▓░      ████
- █▓░▒▓█▓ █▓ ░█▒ ███
-  ██░░▓███▒  █████
-  ██████▓█   ▒██▓▓
-  █▒░░▒█▓   ▒██▒▓▓
-   ██▓▒█▓░██▒██▓██
-     ███████████
-""",
-    """
-  ▓▓▒▒████▓▓▓▓█
-  █▓   ░▒▓░░  ███
-    ██░░    ░░ ░██
-▒░▒▒ ▒█▒  ░▒▒▓░▓██▒
-▒░   ░▓▓▓▒   ▓  █░▒
-▒░░░ ░ ░▒▒▒▓▓▓▒▓█▒▓
- ▒▓░▓███▓░  ░  ░█ ░
-  ▓▓▒░   █▒▒ ▒█▓░██
-  ▓▓▒   ▒▓▓▒ ░█▓░█
-   █████████████
-""",
-    """
-   ▒▒         █▓
- ▒▒  ▓█▓▓██░░░▒▒▓▓
-▒▒░░░▒░░█▓▒░  ░█▒▓█
-    ██▓▒▒███▓░█████
-   ▒▒█▒  ░▒▓██████
-░   ░▒▓▓▓▓  ▓ ██░ ░
-█▓▒▒░▓██▒▒    ██░▓█
- ▓█▒▒ ░▒▓▒░░ ░▓███
-  ▒▒▒░▒░░▓▓▒ ▓█▓▓
-     ███████████
-""",
-]
-
-
-WIZARDS_DATA = [
-    {
-        "name": "Oldspella",
-        "art": WIZARDS_ART[0],
-        "small_art": WIZARDS_ART_SMALL[0],
-        "color": "bright_white",
-        "starting_lives": 5,
-        "combo_requirement": None,
-        "powerup_name": "I Am Enough",
-        "powerup_desc": "<No powerup>",
-        "description": "I am an old soul with the bravado of a new journeyman. Although I may have no powerups, I am familiar with the words of this world, and I prefer to rely solely on my knowledge of the texts to save it.\n\n(This wizard contains no power-ups for players who prefer the classic Worderly Place gameplay.)",
-    },
-    {
-        "name": "Wizard Dict",
-        "art": WIZARDS_ART[1],
-        "small_art": WIZARDS_ART_SMALL[1],
-        "color": "magenta",
-        "starting_lives": 4,
-        "combo_requirement": 3,
-        "powerup_name": "Hardbound Dict",
-        "powerup_desc": "Immune to damage from incorrect guesses for the next 2 turns.",
-        "description": "Yes... the rumors are indeed true. I used to be a dictionary, but a wizard turned me into a human. Words are literally and figuratively inside me... so it'll take a lot more to get me down.\n\n(This wizard is a bit more tough so he can defend himself from life reduction for one turn. Being a dictionary in your former life gives you a lot of pages to rip.)",
-    },
-    {
-        "name": "Streambinder",
-        "art": WIZARDS_ART[2],
-        "small_art": WIZARDS_ART_SMALL[2],
-        "color": "blue",
-        "starting_lives": 3,
-        "combo_requirement": 3,
-        "powerup_name": "Tide of Renewal",
-        "powerup_desc": "Restore +1 Life.",
-        "description": "Greetings, traveler! I am a wizard devoted to healing the sick. With each cure I master, I edge closer to mending myself from this grueling ordeal.\n\n(This wizard has the option to heal himself but starts with less life since. He’s cared so much for others, he had forgotten to care for himself...)",
-    },
-    {
-        "name": "Fyaspella",
-        "art": WIZARDS_ART[3],
-        "small_art": WIZARDS_ART_SMALL[3],
-        "color": "red",
-        "starting_lives": 4,
-        "combo_requirement": 2,
-        "powerup_name": "Blazing Glyphs",
-        "powerup_desc": "Reveal 1 random word on the board.",
-        "description": "RAHHHH! I am a young and upcoming top student of the fya arts, and I’ve been quite the blaze! Gettin' rewarded for consistently stoking the flames, always, always, always!\n\n(This wizard is for those who prefer the pressure of consistently getting it right in order to get a big pay-off. Being a young wizard, you start with less health.)",
-    },
-    {
-        "name": "Lettraseeker",
-        "art": WIZARDS_ART[4],
-        "small_art": WIZARDS_ART_SMALL[4],
-        "color": "green",
-        "starting_lives": 4,
-        "combo_requirement": 4,
-        "powerup_name": "Wildgrowth",
-        "powerup_desc": "Reveal 5-8 letters on the board.",
-        "description": "Ah, the garden of words bloom my curiosity. I am a wizard obsessed with the pieces of the puzzle, rather than the puzzle itself. Each letter is a mystical seed I've sown, scattering them across the soil of thought to let this garden grow.\n\n(This wizard is for those who prefer to compound a lead with a potential powerup. However, the dedication to such powerful magic makes you start with less life.)",
-    },
-]
-
-
-# colors = ["bright_white", "magenta", "blue", "red", "green"]
-# for idx, wizard in enumerate(WIZARDS_ART):
-#     print_message(wizard.strip("\n"), style=colors[idx])
-
-# colors = ["bright_white", "magenta", "blue", "red", "green"]
-# for idx, wizard in enumerate(WIZARDS_ART_SMALL):
-#     print_message(wizard.strip("\n"), style=colors[idx])
