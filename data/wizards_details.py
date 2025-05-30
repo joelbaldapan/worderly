014be5f0c136dbc702bf9acc90b2694bfc50559b
joelbaldapan <baldapan.joelangelo@gmail.com>
2025-05-30 16:43:54 +0800

    fix: update typo errors

--- data/wizards_details.py
+++ data/wizards_details.py
@@ -26,10 +26,10 @@ WIZARDS_DATA: list[WizardData] = [
         combo_requirement=None,
         powerup_name="I Am Enough",
         powerup_desc="<No powerup>",
-        description="I am an old soul with the bravado of a new journeyman. Although I may have no powerups, \
-          I am familiar with the words of this world, and I prefer to rely solely on my knowledge of the \
-          texts to save it.\n\n(This wizard contains no power-ups for players who prefer the classic \
-          Worderly Place gameplay.)",
+        description="I am an old soul with the bravado of a new journeyman. Although I may have no powerups, " \
+        "I am familiar with the words of this world, and I prefer to rely solely on my knowledge of the " \
+        "texts to save it.\n\n(This wizard contains no power-ups for players who prefer the classic " \
+        "Worderly Place gameplay.)",
     ),
     WizardData(
         name="Wizard Dict",
@@ -40,10 +40,10 @@ WIZARDS_DATA: list[WizardData] = [
         combo_requirement=3,
         powerup_name="Hardbound Dict",
         powerup_desc="Immune to damage from incorrect guesses for the next 2 turns.",
-        description="Yes... the rumors are indeed true. I used to be a dictionary, but a wizard turned me \
-          into a human. Words are literally and figuratively inside me... so it'll take a lot more to get \
-          me down.\n\n(This wizard is a bit more tough so he can defend himself from life reduction for \
-          one turn. Being a dictionary in your former life gives you a lot of pages to rip.)",
+        description="Yes... the rumors are indeed true. I used to be a dictionary, but a wizard turned me " \
+        "into a human. Words are literally and figuratively inside me... so it'll take a lot more to get " \
+        "me down.\n\n(This wizard is a bit more tough so he can defend himself from life reduction for " \
+        "one turn. Being a dictionary in your former life gives you a lot of pages to rip.)",
     ),
     WizardData(
         name="Streambinder",
@@ -54,10 +54,10 @@ WIZARDS_DATA: list[WizardData] = [
         combo_requirement=3,
         powerup_name="Tide of Renewal",
         powerup_desc="Restore +1 Life.",
-        description="Greetings, traveler! I am a wizard devoted to healing the sick. With each cure I \
-          master, I edge closer to mending myself from this grueling ordeal.\n\n(This wizard has the \
-            option to heal himself but starts with less life since. He's cared so much for others, he \
-              had forgotten to care for himself...)",
+        description="Greetings, traveler! I am a wizard devoted to healing the sick. With each cure I " \
+        "master, I edge closer to mending myself from this grueling ordeal.\n\n(This wizard has the option " \
+        "to heal himself but starts with less life since. He's cared so much for others, he had forgotten to " \
+        "care for himself...)",
     ),
     WizardData(
         name="Fyaspella",
@@ -68,11 +68,10 @@ WIZARDS_DATA: list[WizardData] = [
         combo_requirement=2,
         powerup_name="Blazing Glyphs",
         powerup_desc="Reveal 1 random word on the board.",
-        description="RAHHHH! I am a young and upcoming top student of the fya arts, and I've been \
-          quite the blaze! Gettin' rewarded for consistently stoking the flames, always, always, \
-          always!\n\n(This wizard is for those who prefer the pressure of consistently getting \
-          it right in order to get a big pay-off. Being a young wizard, you start with less \
-          health.)",
+        description="RAHHHH! I am a young and upcoming top student of the fya arts, and I've been " \
+        "quite the blaze! Gettin' rewarded for consistently stoking the flames, always, always, " \
+        "always!\n\n(This wizard is for those who prefer the pressure of consistently getting " \
+        "it right in order to get a big pay-off. Being a young wizard, you start with less health.)",
     ),
     WizardData(
         name="Lettraseeker",
@@ -83,10 +82,9 @@ WIZARDS_DATA: list[WizardData] = [
         combo_requirement=4,
         powerup_name="Wildgrowth",
         powerup_desc="Reveal 5-8 letters on the board.",
-        description="Ah, the garden of words bloom my curiosity. I am a wizard obsessed with the \
-          pieces of the puzzle, rather than the puzzle itself. Each letter is a mystical seed I've \
-          sown, scattering them across the soil of thought to let this garden grow.\n\n(This wizard \
-          is for those who prefer to compound a lead with a potential powerup. However, the dedication \
-          to such powerful magic makes you start with less life.)",
+        description="Ah, the garden of words bloom my curiosity. I am a wizard obsessed with the pieces of the puzzle, rather " \
+        "than the puzzle itself. Each letter is a mystical seed I've sown, scattering them across the soil of thought " \
+        "to let this garden grow.\n\n(This wizard is for those who prefer to compound a lead with a potential powerup. " \
+        "However, the dedication to such powerful magic makes you start with less life.)",
     ),
 ]
